ed69ac4dc29c4694f18ec69e4c02061403d2f78b
Piyush Prateek <piyushprateek231996@gmail.com>
2023-09-09 00:52:48 +0530

    first test

--- tests/myfirsttestspec.ts
+++ tests/myfirsttestspec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test("My first test", ({ page }) => {
-    page.goto("https://www.google.co.in/")
+test("My first test", async ({ page }) => {
+    await page.goto("https://www.google.co.in/")
+    await expect(page).toHaveTitle("Google")
 })
